0821e67bb322926b87744cc7ce445687540a7a30
Qing Liu <qliu2018@gmail.com>
2025-02-04 21:21:52 -0500

    save

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ import os
 
 version_folder = os.path.dirname(os.path.join(os.path.abspath(__file__)))
 
+
 with open(os.path.join(version_folder, 'verl/version/version')) as f:
     __version__ = f.read().strip()
 
